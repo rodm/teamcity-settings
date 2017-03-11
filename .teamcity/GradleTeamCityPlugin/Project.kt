7b848e74c66e4c7e0775d4b0c636643f0f89f080
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2017-03-11 15:39:22 +0000

    Add Kotlin project settings generated by TeamCity

---
+++ .teamcity/GradleTeamCityPlugin/Project.kt
@@ -0,0 +1,11 @@
+package GradleTeamCityPlugin
+
+import jetbrains.buildServer.configs.kotlin.v10.*
+import jetbrains.buildServer.configs.kotlin.v10.Project
+
+object Project : Project({
+    uuid = "2c4c777e-bf5d-4eaf-8e46-eea999fdbd89"
+    extId = "GradleTeamCityPlugin"
+    parentId = "_Root"
+    name = "Gradle TeamCity Plugin"
+})
