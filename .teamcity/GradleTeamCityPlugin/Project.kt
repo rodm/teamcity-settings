e1bcfcc549e8bdd747690839609e3a87c2191ceb
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2017-03-11 16:12:42 +0000

    Add the VCS root for the settings repository

--- .teamcity/GradleTeamCityPlugin/Project.kt
+++ .teamcity/GradleTeamCityPlugin/Project.kt
@@ -2,6 +2,7 @@ package GradleTeamCityPlugin
 
 import jetbrains.buildServer.configs.kotlin.v10.*
 import jetbrains.buildServer.configs.kotlin.v10.Project
+import jetbrains.buildServer.configs.kotlin.v10.vcs.GitVcsRoot
 
 object Project : Project({
     uuid = "2c4c777e-bf5d-4eaf-8e46-eea999fdbd89"
@@ -9,4 +10,11 @@ object Project : Project({
     parentId = "_Root"
     name = "Gradle TeamCity Plugin"
     description = "Gradle TeamCity Plugin project"
+
+    vcsRoot(GitVcsRoot({
+        uuid = "723408f3-cc0c-42da-b348-dedd4bc030ef"
+        extId = "TeamcitySettings"
+        name = "teamcity-settings"
+        url = "https://github.com/rodm/teamcity-settings"
+    }))
 })
