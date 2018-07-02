41d06a96b2f2eb440285528cd3101f7b72e03ab6
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2018-07-02 15:20:52 +0100

    Add branch name to project description

--- .teamcity/GradleTeamCityPlugin/settings.kts
+++ .teamcity/GradleTeamCityPlugin/settings.kts
@@ -33,7 +33,7 @@ project {
     uuid = "2c4c777e-bf5d-4eaf-8e46-eea999fdbd89"
     extId = "GradleTeamCityPlugin"
     name = "Gradle TeamCity Plugin"
-    description = "Gradle plugin for developing TeamCity plugins [master]"
+    description = "Gradle plugin for developing TeamCity plugins [TeamCity-10.0.x]"
 
     val settingsVcs = GitVcsRoot({
         uuid = "723408f3-cc0c-42da-b348-dedd4bc030ef"
