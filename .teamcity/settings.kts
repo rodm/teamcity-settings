661c9afd1348fac2c052ffa1be94db7a16ee1bcf
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2020-01-10 16:56:24 +0000

    Remove unused import

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -4,7 +4,6 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.project
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
-import jetbrains.buildServer.configs.kotlin.v2019_2.DslContext
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.VcsTrigger.QuietPeriodMode.USE_DEFAULT
 
 import com.github.rodm.teamcity.pipeline
