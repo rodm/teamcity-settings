8d2d7b59c44e346aed976460c3db6af8d2d23c44
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2020-01-11 15:53:14 +0000

    Switch Gradle build scripts to using Kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+
+import com.github.rodm.teamcity.TeamCityEnvironment
+
+plugins {
+    id ("com.github.rodm.teamcity-environments") version "1.2.2"
+}
+
+extra["downloadsDir"] = project.findProperty("downloads.dir") as String? ?: "$rootDir/downloads"
+extra["serversDir"] = project.findProperty("servers.dir") as String? ?: "$rootDir/servers"
+extra["java8Home"] = project.findProperty("java8.home") as String? ?: "/opt/jdk1.8.0_202"
+
+teamcity {
+    environments {
+        downloadsDir = extra["downloadsDir"] as String
+        baseHomeDir = extra["serversDir"] as String
+
+        operator fun String.invoke(block: TeamCityEnvironment.() -> Unit) = environments.create(this, closureOf(block))
+
+        "teamcity2019.2" {
+            version = "2019.2"
+            javaHome = file(extra["java8Home"] as String)
+        }
+    }
+}
